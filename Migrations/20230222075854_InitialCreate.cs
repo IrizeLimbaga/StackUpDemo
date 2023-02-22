cf82410f77ff6b8e53603b489bc4290153b24033
IrizeLimbaga <126058638+IrizeLimbaga@users.noreply.github.com>
2023-02-22 16:56:39 +0800

    Add files via upload

---
+++ Migrations/20230222075854_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StackUpDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatusMaps",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntityStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusMaps", x => x.StatusId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "StatusMaps",
+                columns: new[] { "StatusId", "EntityStatus" },
+                values: new object[,]
+                {
+                    { 1, "Active" },
+                    { 2, "InActive" },
+                    { 3, "Pending" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusMaps");
+        }
+    }
+}
